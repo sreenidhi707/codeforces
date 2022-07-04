4cb6a198f5d242573fc19f195d393514c2097c87
Sreenidhi Anand <sreenidhi707@gmail.com>
2022-07-04 20:32:01 +0000

    problem/160/A

---
+++ problems/160A_twins/160A_twins.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm> // needed for std::sort()
+#include <numeric>   // needed for std::accumulate()
+
+/*
+URL
+
+PROBLEM TITLE
+
+time limit per test  : 1 second
+memory limit per test: 256 megabytes
+input                : standard input
+output               : standard output
+
+PROBLEM STATEMENT
+*/
+
+int problem_160A_twins(std::vector<int>& coinValues) {
+  // sort in descending order
+  std::sort(coinValues.rbegin(), coinValues.rend());
+
+  // find the sum of all the coin values
+  int sum = std::accumulate(coinValues.begin(), coinValues.end(), 0);
+
+  size_t numCoinsTaken = 0, valueTaken = 0, i = 0;
+  size_t valueRemaining = sum;
+  while(i < coinValues.size() and valueTaken <= valueRemaining) {
+    valueTaken += coinValues[i];
+    valueRemaining -= coinValues[i];
+    numCoinsTaken++;
+    i++;
+  }
+
+  return numCoinsTaken;
+}
+
+int main() {
+  size_t numCoins;
+  std::cin >> numCoins;
+
+  std::vector<int> coinValues;
+  while(numCoins--) {
+    int coinValue = 0;
+    std::cin >> coinValue;
+    coinValues.push_back(coinValue);
+  }
+
+  size_t result = problem_160A_twins(coinValues);
+
+  std::cout << result << std::endl;
+}
