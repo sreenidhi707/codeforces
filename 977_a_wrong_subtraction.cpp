c06caaef6f5f440dec9bcf95e20b74b93b954527
Sreenidhi Anand <sreenidhi707@gmail.com>
2022-07-04 10:12:38 -0700

    problem/977/A

---
+++ 977_a_wrong_subtraction.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+
+/*
+https://codeforces.com/problemset/problem/977/A
+A. Wrong Subtraction
+time limit per test1 second
+memory limit per test256 megabytes
+input: standard input
+output: standard output
+
+Little girl Tanya is learning how to decrease a number by one, but she does it wrong with
+a number consisting of two or more digits. Tanya subtracts one from a number by the
+following algorithm:
+
+* if the last digit of the number is non-zero, she decreases the number by one;
+* if the last digit of the number is zero, she divides the number by 10
+  (i.e. removes the last digit).
+
+You are given an integer number n. Tanya will subtract one from it k times. Your task is
+to print the result after all k subtractions.
+
+It is guaranteed that the result will be positive integer number.
+
+Input
+The first line of the input contains two integer numbers n and k (2≤n≤109, 1≤k≤50) — the
+number from which Tanya will subtract and the number of subtractions correspondingly.
+
+Output
+Print one integer number — the result of the decreasing n by one k times.
+
+It is guaranteed that the result will be positive integer number.
+*/
+
+uint64_t problem_977(uint64_t n, uint64_t k) {
+  while(k-- && n) {
+    if(n % 10) {
+      n--;
+    }
+    else {
+      n /= 10;
+    }
+  }
+  return n;
+}
+
+int main() {
+  std::string line;
+  std::getline(std::cin, line);
+
+  std::stringstream ss(line);
+
+  std::string k, n;
+  std::getline(ss, n, ' ');
+  std::getline(ss, k, ' ');
+
+  int result = problem_977(std::atoi(n.c_str()), std::atoi(k.c_str()));
+  std::cout << result;
+}
