a011b40bb1e841e58bc2ea243414e293e1a1f33d
Sreenidhi Anand <sreenidhi707@gmail.com>
2022-07-04 19:39:40 +0000

    Add template folder

---
+++ problems/template/96A_football.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <iostream>
+
+/*
+URL
+
+PROBLEM TITLE
+
+time limit per test  : 1 second
+memory limit per test: 256 megabytes
+input                : standard input
+output               : standard output
+
+PROBLEM STATEMENT
+*/
+
+bool problem_number() {
+
+  return false;
+}
+
+int main() {
+  std::string arguments;
+  std::cin >> arguments;
+
+  bool result = problem_number();
+
+  std::cout << result << std::endl;
+}
